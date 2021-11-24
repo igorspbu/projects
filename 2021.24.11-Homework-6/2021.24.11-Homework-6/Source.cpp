a5a817f65162163495f8898fc8b2950a3344d454
igorspbu <iorlovskiy29@yandex.ru>
2021-11-24 20:11:06 +0300

    stack
    ?:почему f1 и f2 не работают вместе

---
+++ 2021.24.11-Homework-6/2021.24.11-Homework-6/Source.cpp
@@ -0,0 +1,114 @@
+#include<iostream>
+#include<string>
+#include<stack>
+
+using namespace std;
+
+int f2()
+{
+	char x;
+	int num;
+	stack<int>s;
+	int a = 0;
+	int b = 0;
+	while (cin >> x)
+	{
+		if (isdigit(x))
+		{
+			cin.putback(x);
+			cin >> num;
+			s.push(num);
+		}
+		if (x == '*' || x == '/' || x == '+' || x == '-')
+		{
+			a = s.top();
+			s.pop();
+			b = s.top();
+			s.pop();
+			if (x == '*')
+			{
+				s.push(a * b);
+			}
+			else if (x == '/')
+			{
+				s.push(b / a);
+			}
+			else if (x == '+')
+			{
+				s.push(a + b);
+			}
+			else if (x == '-')
+			{
+				s.push(b - a);
+			}
+		}
+	}
+	return s.top();
+}
+
+string f1()
+{
+	stack<char>sym;
+	char x;
+	string s;
+	int number;
+	char symbol;
+
+	while (cin >> x)
+	{
+		if (isdigit(x))
+		{
+			cin.putback(x);
+			cin >> number;
+			s += to_string(number);
+		}
+		if (x == '+' || x == '-' || x == '(')
+		{
+			cin.putback(x);
+			cin >> symbol;
+			sym.push(symbol);
+		}
+		else if (x == '*' || x == '/')
+		{
+			if (sym.top() == '*' || sym.top() == '/')
+			{
+				s += sym.top();
+				sym.pop();
+			}
+			cin.putback(x);
+			cin >> symbol;
+			sym.push(symbol);
+		}
+		else if (x == ')')
+		{
+			while (sym.top() != '(')
+			{
+				s += sym.top();
+				sym.pop();
+			}
+			sym.pop();
+		}
+
+	}
+	if (!sym.empty())
+	{
+		while (!sym.empty())
+		{
+			s += sym.top();
+			sym.pop();
+		}
+	}
+	return s;
+}
+
+int main()
+{
+	//3+4*2/(1-5)
+	//3 4 2 * 1 5 - / +
+	setlocale(LC_ALL, "Russian");
+	cout << "Обратная польская запись выражения: " << f1() << endl;
+	cout << "Значение выражения, записанного в ОПЗ: " << f2() << endl;
+
+
+	return EXIT_SUCCESS;
+}
